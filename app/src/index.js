91ab1141cd55074862247ae90722f4b35ea24f91
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-10 15:50:43 -0400

    add name title input with useState. Add component to the root container and wired up the application

--- app/src/index.js
+++ app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Playground from './Playground';
+import App from '../src/container/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Playground />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
