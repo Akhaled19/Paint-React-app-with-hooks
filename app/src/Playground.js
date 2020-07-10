08ab4f7c5e37ccec5072aa26a061649617295f19
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-09 20:33:39 -0400

    add setState hook example

--- app/src/Playground.js
+++ app/src/Playground.js
@@ -1,2 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import randomColor from 'randomcolor';
+
+export default function Playground() {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            {count}
+            <button onClick={()=> setCount(currentCount => currentCount - 1)}>-</button>
+            <button onClick={ () => setCount(currentCount => currentCount + 1)}>+</button>
+        </div>
+    )
+};
