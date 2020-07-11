2f643cc9608bbdef2f35157c6c14c9a1691b4da1
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-11 18:56:52 -0400

    add useCallback hook to count how many tiimes page rerenders

--- app/src/Playground.js
+++ app/src/Playground.js
@@ -1,24 +1,30 @@
-import React, { useState, useEffect, useRef } from 'react';
-import randomColor from 'randomcolor';
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react'
+import randomColor from 'randomcolor'
 
 export default function Playground() {
-    const [count, setCount] = useState(30);
+  const [count, setCount] = useState(30)
+  
+  const inputRef = useRef()
+  
+  const [color, setColor] = useState(randomColor())
+  useEffect(() => inputRef.current.focus(), [count])
+  
+  const calculate = useCallback(<Calculate />, [count])
+  
+  return (
+    <div style={{ borderTop: `10px solid ${color}`}}>
+      {count}
+      <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
+      <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button>
+      <button onClick={() => setColor(randomColor())}>Change Color</button>
+      <hr />
+      <input ref={inputRef} type="range" onChange={e => setCount(e.target.value)} value={count} />
+      {calculate}
+    </div>
+  )
+}
 
-    const inputRef = useRef();
-
-    const [color, setColor] = useState(null);
-    useEffect( () => { 
-        setColor(randomColor())
-        inputRef.current.focus() 
-    }, [count]);
-
-    return (
-        <div style={{borderTop: `10px dashed ${color}`}}>
-            {count}
-            <button onClick={()=> setCount(currentCount => currentCount - 1)}>-</button>
-            <button onClick={ () => setCount(currentCount => currentCount + 1)}>+</button>
-            <br />
-            <input ref={inputRef} type="range" onChange={e => setCount(e.target.value)} value={count} />
-        </div>
-    )
-};
+function Calculate() {
+  const renderCount = useRef(1)
+  return <div>{renderCount.current ++}</div>
+}
--- app/src/index.js
+++ app/src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Playground />
   </React.StrictMode>,
   document.getElementById('root')
 );
