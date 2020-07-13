09db3271beff37cc75f5d933c340fbe09b6bad84
Akhaled19 <56312360+Akhaled19@users.noreply.github.com>
2020-07-12 23:06:06 -0400

    add & update code to convert the window function to a custom hook

--- app/src/components/WindowSize.js
+++ app/src/components/WindowSize.js
@@ -1,22 +1,17 @@
 import React, {useState, useEffect} from 'react';
 
-export default function WindowSize() {
+export default function useWindowSize(cb) {
     const [[windowWidth, windowHight], setWindowSize] = useState([window.innerHeight, window.innerWidth]);
-    const [visible, setVisible] = useState(false);
+    
     useEffect(() => {
-        let timeoutId;
         const handleResize = () => {
+            cb()
             setWindowSize([window.innerWidth, window.innerHeight])
-            setVisible(true)
-            clearTimeout(timeoutId)
-            timeoutId = setTimeout(() => setVisible(false), 100)
         }
         window.addEventListener('resize', handleResize)
         return () => window.removeEventListener('resize', handleResize)
     }, [])
     return (
-        <div className={`window-size ${visible ? '' : 'hidden'}`}>
-            {windowWidth} x {windowHight}
-        </div>
+       [windowWidth, windowHight]
     )
 } 
